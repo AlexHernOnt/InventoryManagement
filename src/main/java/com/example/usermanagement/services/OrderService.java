73b65d641b70da38f199c396242e063ce59aeac1
Sergio <sergio.solabre@qaracter.com>
2025-02-12 10:54:36 +0000

    feat: change order status

--- src/main/java/com/example/usermanagement/services/OrderService.java
+++ src/main/java/com/example/usermanagement/services/OrderService.java
@@ -1,6 +1,8 @@
 package com.example.usermanagement.services;
 
 import com.example.usermanagement.models.Order;
+import com.example.usermanagement.models.OrderProduct;
+import com.example.usermanagement.models.State;
 import com.example.usermanagement.repositories.OrderProductRepository;
 import com.example.usermanagement.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +34,18 @@ public class OrderService {
     public Order createOrder(Order order){ return null; }
 
     public Order changeStatus(Long id, String status){
-    return null;
+        Order order = this.orderRepository.findById(id).orElse(null);
+        if(status.equals(State.COMPLETED)) {
+
+            if (order != null) {
+                for (OrderProduct orderProduct : order.getOrderproducts()) {
+                    this.productService.reduceStock(orderProduct.getProduct().getId(), orderProduct.getQuantity());
+                }
+            }
+
+        }
+        order.setState(State.valueOf(status));
+        return order;
     }
 }
 
