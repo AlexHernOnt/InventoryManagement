044300a668df17bfb5756182a54f9a99559f0ee3
ML-RefVm-358739\usuario.qaracter <jesus.hernandez@imagina-bpo.es>
2025-02-12 09:21:44 +0000

    feat: Create service for order

--- src/main/java/com/example/usermanagement/controllers/OrderController.java
+++ src/main/java/com/example/usermanagement/controllers/OrderController.java
@@ -1,4 +1,43 @@
 package com.example.usermanagement.controllers;
 
+import com.example.usermanagement.models.Order;
+import com.example.usermanagement.services.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * REST Controller for managing order-related operations.
+ */
+@RestController
+@RequestMapping("/order")
+
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) { this.orderService = orderService; }
+
+    @GetMapping
+    public List<Order> getAllOrders() {return this.orderService.getAllOrders();}
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Order createOrder(@Valid @RequestBody Order order) { return this.orderService.createOrder(order);}
+
+    @GetMapping("/{id}")
+    public Order getOrderById(@PathVariable Long id) {
+        return this.orderService.getOrderById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Order switchStatus(@PathVariable Long id, String status) {
+        return this.orderService.changeStatus(id, status);
+    }
+
 }
+
--- src/main/java/com/example/usermanagement/services/OrderService.java
+++ src/main/java/com/example/usermanagement/services/OrderService.java
@@ -1,4 +1,36 @@
 package com.example.usermanagement.services;
 
+import com.example.usermanagement.models.Order;
+import com.example.usermanagement.models.User;
+import com.example.usermanagement.repositories.OrderProductRepository;
+import com.example.usermanagement.repositories.OrderRepository;
+import com.example.usermanagement.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service class for managing order operations.
+ */
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderProductRepository orderProductRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository, OrderProductRepository orderProductRepository){
+        this.orderRepository = orderRepository;
+        this.orderProductRepository = orderProductRepository;
+    }
+
+    public List<Order> getAllOrders(){ return null; }
+
+    public Order getOrderById(Long id) { return null; }
+
+    public Order createOrder(Order order){ return null; }
+
+    public Order changeStatus(Long id, String status){ return null; }
 }
